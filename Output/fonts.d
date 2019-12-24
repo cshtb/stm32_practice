356c51189a7f8c08618b7f4e60d40c7271fbf541
unknown <759162657@qq.com>
2019-12-24 11:18:04 +0800

    first commit

---
+++ Output/fonts.d
@@ -0,0 +1,3 @@
+..\..\output\fonts.o: ..\..\User\font\fonts.c
+..\..\output\fonts.o: ..\..\User\./font/fonts.h
+..\..\output\fonts.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ Output/mlmath.d
@@ -0,0 +1,2 @@
+..\..\output\mlmath.o: ..\..\User\mpu6050\mllite\mlmath.c
+..\..\output\mlmath.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
